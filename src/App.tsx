05d61575ae5e630c3b4b0e07d65bef97e352e4d1
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-03-06 09:00:33 -0300

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { useCallback, useRef } from 'react';
+import './App.css'
+
+import { usePinch, Handler } from "@use-gesture/react"
+
+function App() {
+  const imgRef = useRef<null | HTMLImageElement>(null)
+
+  const onImageZoom: Handler<"pinch"> = useCallback(({ offset, origin }) => {
+    if (!imgRef.current) return;
+
+    const rect = imgRef.current?.getBoundingClientRect();
+    if (!rect) return;
+    const [scale] = offset
+    const [originX, originY] = origin;
+    const translateFactor = 0.5;
+    const [mappedX, mappedY] = [originX - rect.x, originY - rect.y].map(Math.round).map(n => n * translateFactor);
+
+    imgRef.current.style.transform = `scale(${scale})`;
+    imgRef.current.style.transformOrigin = `${mappedX}px ${mappedY}px`;
+  }, [])
+
+  usePinch(
+    onImageZoom,
+    {
+      target: imgRef,
+      scaleBounds: {
+        max: 2,
+        min: 1,
+      },
+      rubberband: 0.3,
+    }
+  )
+
+  return (
+    <main>
+      <h2>
+        Use gesture example
+      </h2>
+
+      <img
+        ref={imgRef}
+        src="https://picsum.photos/200"
+        style={{
+          width: "100%",
+          height: 'auto'
+        }}
+      />
+    </main>
+  )
+}
+
+export default App
